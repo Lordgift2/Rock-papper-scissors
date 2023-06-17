f6fb59f4a85e19b3b81da841c71d447a3428dad8
lordgift2 <adewunmimathew@gmail.com>
2023-06-17 12:23:19 +0100

    The scoreborad was added to the html, the Js file was modified to use button funtion  on the html useing DOM and all modification was styled on the css file.

--- rock.js
+++ rock.js
@@ -33,12 +33,36 @@ else{
 }
     }
 function game(){
-    let playerSelection = (prompt('Enter selector'));
+    let playerSelection = "";
     let computerSelection = getComputerChoice(selector);
     console.log(playerSelection);
     console.log(computerSelection);
    console.log(playRound(playerSelection,computerSelection));
 }
+ const rockBtn = document.getElementById('rock');
+ rockBtn.addEventListener('click',()=>{
+    playerSelection = selector[0];
+    computerSelection = getComputerChoice(selector);
+
+    console.log(playRound(playerSelection,computerSelection))
+ })
+ const papperBtn = document.getElementById('papper');
+ papperBtn.addEventListener('click',()=>{
+    playerSelection = selector[1];
+    computerSelection = getComputerChoice(selector);
+
+    console.log(playRound(playerSelection,computerSelection))
+ })
+ const scissorsBtn = document.getElementById('scissors');
+ scissorsBtn.addEventListener('click',()=>{
+    playerSelection = selector[2];
+    computerSelection = getComputerChoice(selector);
+
+    console.log(playRound(playerSelection,computerSelection))
+ })
+/*
 for (let i = 0; i <5; i++) {
 game();
 }
+*/
+
