fe7785d789fe95c580e9ae0dca6bd5893e0ddf5a
lordgift2 <adewunmimathew@gmail.com>
2023-07-26 04:06:10 +0100

    The UI pages and score paragraph was updated

--- rock.js
+++ rock.js
@@ -3,66 +3,125 @@
         return computerChoice;
 }
 const selector = ['Rock','Papper','Scissors']
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === selector[0] &&
-        computerSelection === selector[0]||
-        playerSelection === selector[1]&&
-        computerSelection===selector[1]||
-        playerSelection===selector[2]&&
-        computerSelection===selector[2]){
-        return ('It\'s a tie play again');
+    if (playerSelection === computerSelection){
+        roundWinner === 'It\'s a tie play again';
     }
-    else if (playerSelection === selector[0] &&
+     if (playerSelection === selector[0] &&
              computerSelection === selector[1] ||
              playerSelection === selector[1] && 
              computerSelection === selector[2] ||
              playerSelection === selector[2] && 
              computerSelection === selector[0]){
-        return ('Computer win');
+               computerScore++;
+              roundWinner === "computer";
     }
-    else if (playerSelection === selector[1] &&
-        computerSelection === selector[0] ||
-        playerSelection === selector[2] && 
-        computerSelection === selector[1] ||
-        playerSelection === selector[0] && 
-        computerSelection === selector[2]){
-    return('player win')
-}
-else{
-    return('invalid entries selected');
-}
+     if (playerSelection === selector[1] &&
+      computerSelection === selector[0] ||
+      playerSelection === selector[2] && 
+      computerSelection === selector[1] ||
+      playerSelection === selector[0] && 
+      computerSelection === selector[2]) {
+      playerScore++;
+        roundWinner ===  "player";
     }
-function game(){
-    let playerSelection = "";
-    let computerSelection = getComputerChoice(selector);
-    console.log(playerSelection);
-    console.log(computerSelection);
-   console.log(playRound(playerSelection,computerSelection));
+    + updateScoremessage(roundWinner,playerSelection,computerSelection);
 }
+function gameOver(){
+   return playerScore === 5 || computerScore === 5
+}
+ //Game page Info
+ const scoreInfo = document.getElementById('scoreInfo')
+ const scoreMessage = document.getElementById('scoreMessage');
+ const playerScorepara = document.getElementById("playerScore");
+ const computerScorepara = document.getElementById("computerScore");
+ const roundWinnerpara = document.getElementById("roundWinner");
  const rockBtn = document.getElementById('rock');
- rockBtn.addEventListener('click',()=>{
-    playerSelection = selector[0];
-    computerSelection = getComputerChoice(selector);
-
-    console.log(playRound(playerSelection,computerSelection))
- })
  const papperBtn = document.getElementById('papper');
- papperBtn.addEventListener('click',()=>{
-    playerSelection = selector[1];
-    computerSelection = getComputerChoice(selector);
+ const scissorsBtn = document.getElementById('scissors')
+ const restartBtn=document.getElementById("restartBtn");
+ const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const overlay = document.getElementById('overlay');
 
-    console.log(playRound(playerSelection,computerSelection))
- })
- const scissorsBtn = document.getElementById('scissors');
- scissorsBtn.addEventListener('click',()=>{
-    playerSelection = selector[2];
-    computerSelection = getComputerChoice(selector);
+ rockBtn.addEventListener('click',()=>handleclick('Rock'));
+ papperBtn.addEventListener('click',()=>handleclick('Papper'));
+ scissorsBtn.addEventListener('click',()=>handleclick('Scissors'));
+ restartBtn.addEventListener('click',restartGame);
 
-    console.log(playRound(playerSelection,computerSelection))
- })
-/*
-for (let i = 0; i <5; i++) {
-game();
+ function handleclick(playerSelection){
+   if(gameOver()){
+      openEndgameModal()
+      return
+   }
+   const computerSelection = getComputerChoice(selector);
+   playRound(playerSelection,computerSelection);
+   updateScore();
+
+   if(gameOver()){
+      openEndgameModal()
+      setFinalMessage()
+      return
+   }
+ }
+function updateScore(){
+    if(roundWinner ==='It\'s a tie play again'){
+      scoreInfo.textContent = "It's a tie!"
+    } 
+     else if(roundWinner === 'computer'){
+      scoreInfo.textContent = 'You won!'
+   }
+    else if (roundWinner === 'player'){
+      scoreInfo.textContent = 'You lost!'
+   }
+   playerScorepara.textContent=`Playerscore:${playerScore}`;
+   computerScorepara.textContent=`Computerscore:${computerScore}`;
 }
-*/
+function updateScoremessage(winner,playerSelection,computerSelection){
+   if ( winner === "computer"){
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+         computerSelection
+       )}  beat  ${playerSelection.toLowerCase()}`
+       return;
+   }
+  if (winner === "player"){
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+         playerSelection
+       )}  beat  ${computerSelection.toLowerCase()}`
+       return;
+   }
+   
+      //scoreMessage.textContent = `${capitalizeFirstLetter(
+      //   playerSelection
+    //   )} tie  ${computerSelection.toLowerCase()}`
+}
+function capitalizeFirstLetter(string) {
+   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+ }
+ function openEndgameModal() {
+   endgameModal.classList.add('active')
+   overlay.classList.add('active')
+ }
+ function closeEndgameModal() {
+   endgameModal.classList.remove('active')
+   overlay.classList.remove('active')
+ }
+ function setFinalMessage() {
+   return playerScore > computerScore
+     ? (endgameMsg.innerHTML = '<strong>You won</strong> 😀')
+     : (endgameMsg.innerHTML = '<strong>You lost</strong> 🙁')
+     ? (endgameMsg.textContent = 'You won!')
+     : (endgameMsg.textContent = 'You lost...')
+ }
+ function restartGame(){
+    playerScore=0;
+    computerScore=0;
+    playerScorepara.textContent = `Playerscore:${playerScore}`;
+    computerScorepara.textContent=`Computerscore:${computerScore}`;
+    
+ };
 
+    
